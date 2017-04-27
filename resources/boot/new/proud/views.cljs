c27823b7a757c2ac10d64979c54ee06e00f96119
Josef Pospíšil <josef.pospisil@laststar.eu>
2017-04-27 14:24:44 +0200

    Add mdl layout to views

--- resources/boot/new/proud/views.cljs
+++ resources/boot/new/proud/views.cljs
@@ -6,21 +6,26 @@
               [{{name}}.events :as events]))
 
 (rum/defc emitting-button [store label event]
-  (mdl/button
-   {:mdl [:ripple]
-    :on-click #(ptk/emit! store event)}
-   label))
+  (mdl/button {:mdl [:ripple] :on-click #(ptk/emit! store event)} label))
 
 (rum/defc main < rum/reactive [store]
   (let [state (rx/to-atom store)
         button-clicked (rum/react (rum/cursor state :button/clicked))]
-    [:div
-     [:h1 "Fresh {{name}} project "]
-     [:h2
-      "Find the code for this page in "
-      [:code "`src/{{name}}/views.cljs`"]]
-     (if button-clicked
-       [:div
-        [:h3 "Oh, you already figured out the clicking!"]
-        (emitting-button store "Unclick?" (events/->ButtonUnclicked))]
-       (emitting-button store "Click me!" (events/->ButtonClicked)))]))
+    (mdl/layout
+     (mdl/header
+      (mdl/header-row
+       (mdl/layout-title "Fresh {{name}} project ")))
+     (mdl/main-content
+      (mdl/grid
+       (mdl/cell
+        {:mdl [:12]}
+        "Find the code for this page in"
+        [:code "`src/{{name}}/views.cljs`"])
+       (if button-clicked
+         (mdl/cell
+          {:mdl [:12]}
+          [:h3 "Oh, you already figured out the clicking!"]
+          (emitting-button store "Unclick?" (events/->ButtonUnclicked)))
+         (mdl/cell
+          {:mdl [:12]}
+          (emitting-button store "Click me!" (events/->ButtonClicked)))))))))
